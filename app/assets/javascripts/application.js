7f454bd2ddfc5a0c109759cda57bd02c70cb9476
Daniel Springer <dssp@freenet.de>
2018-05-31 22:22:41 +0200

    Finished home page layout and components

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,32 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery3
 //= require_tree .
+
+$(document).ready(function(){
+    animateDiv('.imgPaper');
+    animateDiv('.imgRock');
+    animateDiv('.imgScissors');
+});
+
+function makeNewPosition(){
+
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 250;
+    var w = $(window).width() - 250;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh,nw];
+
+}
+
+function animateDiv(myclass){
+    var newq = makeNewPosition();
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 2400,   function(){
+      animateDiv(myclass);
+    });
+
+};
